6b51833789e863ac7635b045029614ec65266b28
eatu <armanulhai@gmail.com>
2024-09-24 04:46:05 +0600

    history section added
    history section added

--- donate.js
+++ donate.js
@@ -1,15 +1,42 @@
 document.getElementById('btn-add-flood-bdt')
 .addEventListener('click',function(event){
     event.preventDefault();
-
     dntFloodbdt();
+
+   
 })
 
 document.getElementById('btn-palestine-donation')
 .addEventListener('click',function(event){
     event.preventDefault();
-    console.log("hello")
-
-    
+   dntPalestinebdt();
+  
 })
+
+document.getElementById('btn-quota-donation')
+.addEventListener('click',function(event){
+    event.preventDefault();
+    dntQuotabdt();
+  
+})
+
+document.getElementById('btn-history').addEventListener('click',function(event){
+    
+    document.getElementById('donate-section').classList.add('hidden')
+    document.getElementById('History-section ').classList.remove('hidden')
+
+
+})
+document.getElementById('btn-donation').addEventListener('click',function(event){
+    
+    document.getElementById('donate-section').classList.remove('hidden')
+    document.getElementById('History-section ').classList.add('hidden')
+
+
+})
+
+
+
+
+
 
--- function.js
+++ function.js
@@ -7,8 +7,16 @@ function dntFloodbdt(){
     const cFlooddonation=parseFloat(currFlooddonation);
     const newFloodbdt = cAddfloodbdt+cFlooddonation;  
     const remainBalance= myBalance() - cAddfloodbdt;
-    document.getElementById('food-money').innerText=newFloodbdt;
+    document.getElementById('flood-money').innerText=newFloodbdt;
     document.getElementById('my-balance').innerText=remainBalance;
+
+
+    const floodHistory=document.createElement('div');
+    // floodHistory.classList.add('bg-yellow-300');
+    floodHistory.innerHTML = `<div class=" bg-base-100 p-10 rounded  shadow-xl text-xl lg:w-1200  lg:mx20 "> <p> ${newFloodbdt} Taka is Donated for famine-2024 at Feni, Bangladesh <p/></div>`
+    document.getElementById('History-container').appendChild(floodHistory)
+    
+   
 }
 
 
@@ -16,11 +24,31 @@ function dntFloodbdt(){
 
 function dntPalestinebdt(){
 
-    const currPlaestinebdt=document.getElementById('palestine-money').value;
-    console.log('currPlaestinebdt')
-
-
+    const currPlaestinebdt=document.getElementById('palestine-money').innerText;
+    const addPalestiebdt =document.getElementById('add-palestine-money').value;
+    const cPlaestinebdt = parseFloat(currPlaestinebdt);
+    const cAddpalestinebdt= parseFloat(addPalestiebdt);
+    const newPalestinebdt = cPlaestinebdt + cAddpalestinebdt;
+    document.getElementById('palestine-money').innerText=newPalestinebdt;
+    const remainBalance= myBalance() - cAddpalestinebdt;
+    document.getElementById('my-balance').innerText=remainBalance;
+ 
 }
+
+function dntQuotabdt(){
+
+    const currQuotabdt=document.getElementById('quota-balance').innerText;
+    const addQuotabdt =document.getElementById('add-quota money').value;
+    const cQuotabdt = parseFloat(currQuotabdt);
+    const cAddQuotabdt= parseFloat(addQuotabdt);
+    const newQuotabdt =  cQuotabdt + cAddQuotabdt;
+    document.getElementById('quota-balance').innerText=newQuotabdt;
+    const remainBalance= myBalance() - cAddQuotabdt;
+    document.getElementById('my-balance').innerText=remainBalance;
+ 
+}
+
+
 
 function myBalance(){
     const mybdt= document.getElementById('my-balance').innerText;
