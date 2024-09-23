c4acdd4c163e35d364bad03d2a34abb04b4b5e23
eatu <armanulhai@gmail.com>
2024-09-24 02:56:09 +0600

    Update function.js
    addevent in js

--- js/function.js
+++ js/function.js
@@ -1,6 +1,21 @@
 function dntFloodbdt(){
-    const addfloodbdt=document.getElementById('add-flood-bdt').value;
-    const currFlooddonatin=document.getElementById('food-money').innerText;
-    
-    
+
+
+    const addFloodbdt=document.getElementById('add-flood-bdt').value;
+    const currFlooddonation=document.getElementById('food-money').innerText;
+    const cAddfloodbdt=parseFloat(addFloodbdt);
+    const cFlooddonation=parseFloat(currFlooddonation);
+    const newFloodbdt = cAddfloodbdt+cFlooddonation;  
+    const remainBalance= myBalance() - cAddfloodbdt;
+    document.getElementById('food-money').innerText=newFloodbdt;
+    document.getElementById('my-balance').innerText=remainBalance;
+}
+
+
+
+
+function myBalance(){
+    const mybdt= document.getElementById('my-balance').innerText;
+    const cmyBalance= parseFloat(mybdt);
+    return cmyBalance;
 }
